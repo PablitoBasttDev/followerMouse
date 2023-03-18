ced5287e5e885ee6506d6d7a71b9a3100f1eee62
PABLO GIORDANO <pablogiordano97@gmail.com>
2023-03-17 23:51:41 -0300

    Follower mouse useEffect

---
+++ src/App.jsx
@@ -0,0 +1,55 @@
+import { useEffect, useState } from 'react'
+
+const FollowMouse = () => {
+  const [enabled, setEnabled] = useState(false)
+  const [position, setPosition] = useState({ x: 0, y: 0 })
+
+  // Los hooks (useEffect, useState, etc) van siempre en el cuerpo del componente y nunca dentro de un if o ciclo
+  useEffect(() => {
+    console.log('efecto', { enabled })
+
+    const handleMove = (event) => {
+      const { clientX, clientY } = event
+      console.log('handleMove', { clientX, clientY })
+      setPosition({ x: clientX, y: clientY })
+    }
+    if (enabled) {
+      window.addEventListener('pointermove', handleMove)
+    }
+    // Esto se ejecuta cuando cambia la dependencia (en este caso enabled) o cuando se desmonta el componente
+    return () => {
+      window.removeEventListener('pointermove', handleMove)
+    }
+  }, [enabled])
+
+  return (
+    <>
+      <div style={{
+        position: 'absolute',
+        backgroundColor: '#09f',
+        borderRadius: '50%',
+        opacity: 0.8,
+        pointerEvents: 'none',
+        left: -20,
+        top: -20,
+        width: 40,
+        height: 40,
+        transform: `translate(${position.x}px, ${position.y}px)`
+      }}
+      />
+      <button onClick={() => setEnabled(!enabled)}>
+        {enabled ? 'Desactivar' : 'Activar'} seguir el puntero
+      </button>
+    </>
+  )
+}
+
+function App () {
+  return (
+    <main>
+      <FollowMouse />
+    </main>
+  )
+}
+
+export default App
